9f3407090e838ed6d60325bffc17eda0c997f12f
Nataniel Mendes <natanielmendes@hotmail.com>
2022-07-27 19:25:48 +0100

    Adding A Checkout Form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 
 import Modal from '../UI/Modal'
 import CartItem from './CartItem'
 import classes from './Cart.module.css'
 import CartContext from '../../store/cart-context'
+import Checkout from './Checkout'
 
-const Cart = props => {
+const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false)
   const cartContext = useContext(CartContext)
 
   const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`
@@ -14,9 +16,13 @@ const Cart = props => {
   const cartItemRemoveHandler = (id) => {
     cartContext.removeItem(id)
   }
-  
+
   const cartItemAddHandler = (item) => {
-    cartContext.addItem({...item, amount: 1})
+    cartContext.addItem({ ...item, amount: 1 })
+  }
+
+  const orderHandler = () => {
+    setIsCheckout(true)
   }
 
   const cartItems = (
@@ -30,21 +36,34 @@ const Cart = props => {
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
         />
-      ))}  
+      ))}
     </ul>
   )
 
-  return <Modal onClose={props.onClose}>
-    {cartItems}
-    <div className={classes.total}>
-      <span>Total amount</span>
-      <span>{totalAmount}</span>
-    </div>
+  const modalActions = (
     <div className={classes.actions}>
-      <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-      {hasItems && <button className={classes.button}>Order</button>}
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
     </div>
-  </Modal>
+  )
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {!isCheckout && modalActions}
+    </Modal>
+  )
 }
 
 export default Cart
